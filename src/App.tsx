76298397b6bce4a4e2c3cfae6b78efc2f6eae3f7
nate-hunter <hunter.j.nate@gmail.com>
2024-07-27 05:57:07 -0400

    project creation

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from './components/Buttons';
+
+function App() {
+    return (
+        <>
+            <h1>LILIKOI-UI</h1>
+
+            <div>
+                <h3>Test Button:</h3>
+                <Button>Click Me</Button>
+            </div>
+        </>
+    );
+}
+
+export default App;
