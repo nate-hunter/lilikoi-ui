8f305eda64927db1888a9c11a9a039d518fcf687
nate-hunter <hunter.j.nate@gmail.com>
2024-09-22 17:32:22 -0400

    updated global stylesheet + imports for local testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export { Button } from './components/Buttons/Button';
+import './styles.css';
+
+// export Button  from './components/button';
+export * from './components/button';
+export { Pill } from './components/Pills/Pill';
+
+import * as SNEK from './snek-component-library';
+export { SNEK };
