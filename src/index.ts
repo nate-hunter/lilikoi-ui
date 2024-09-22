b3aa64c99989261adeb18566896d93d8ebbab4d0
nate-hunter <hunter.j.nate@gmail.com>
2024-09-22 18:58:37 -0400

    updated top level `index.ts` file to only export `Button`

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import './styles.css';
 
-// export Button  from './components/button';
-export * from './components/button';
-export { Pill } from './components/Pills/Pill';
+export * from './components/Button';
+// export { Pill } from './components/Pills/Pill';
 
-import * as SNEK from './snek-component-library';
-export { SNEK };
+// import * as SNEK from './snek-component-library';
+// export { SNEK };
