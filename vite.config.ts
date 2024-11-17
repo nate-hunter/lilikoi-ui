316d8b144a7b977e11be6f6192b48568d9bf6505
nate-hunter <hunter.j.nate@gmail.com>
2024-11-17 02:57:28 -0500

    updated typescript + vite config files to resolve build issues

--- vite.config.ts
+++ vite.config.ts
@@ -7,21 +7,21 @@ import dts from 'vite-plugin-dts';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), dts({ rollupTypes: true })],
-    build: {
-        lib: {
-            entry: resolve(__dirname, './src/index.ts'),
-            name: 'LilikoiUI',
-            fileName: 'lilikoi-ui',
-        },
-        rollupOptions: {
-            external: ['react', 'react-dom', 'react/jsx-runtime'],
-            output: {
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM',
-                    'react/jsx-runtime': 'react/jsx-runtime',
-                },
-            },
-        },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'LilikoiUI',
+      fileName: 'lilikoi-ui',
     },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime',
+        },
+      },
+    },
+  },
 });
